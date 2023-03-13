21ae4e95941ba4e65d272e69d053cb8078c5958b
CRF450Fanboy <119955429+CRF450Fanboy@users.noreply.github.com>
2023-03-13 07:19:55 +0100

    first release
    no working functions atm

---
+++ main.py
@@ -0,0 +1,16 @@
+import os
+from colorama import Fore, Back, Style #pip install colorama
+
+def main() :
+    print(Fore.YELLOW + "     ____  _________")
+    print("    |    |/   _____/")
+    print("    |    |\_____  \ ")
+    print("/\\__|    |/        \\")
+    print("\\________/_______  /")
+    print("                 \\/ ")
+    print("")
+    print("Remove console.log(\"\") from JavScript file" + Fore.RESET)
+
+if __name__ == "__main__" :
+    os.system("")
+    main()
